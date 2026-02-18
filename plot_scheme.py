be182fb1088a0344de23d4d1948032d5f97a2324
eliasarnqvist <elias.arnqvist@physics.uu.se>
2026-02-18 08:05:13 +0100

    added parent, added save figure

--- plot_scheme.py
+++ plot_scheme.py
@@ -15,6 +15,7 @@ def parse_ensdf_levels_gammas(path, nucid):
     nucid = nucid[:5].upper()
     # Dictionary of the nuclear levels
     levels = {}
+    parent = {}
 
     with open(path, "r", encoding="utf-8", errors="ignore") as f:
         # Read all lines and store them
@@ -26,17 +27,26 @@ def parse_ensdf_levels_gammas(path, nucid):
         line = line.rstrip("\n")
         # print(line)
 
-        # Make sure the nucid is the same
-        if line[:5].upper() != nucid:
-            continue
-
         # This is the record identifier (ex. L=level, G=gamma)
         rec = line[5:8].strip()
         # print(rec, line)
 
+        # Look for the parent first
+        if rec == "P":
+            # Energy in keV
+            E_p = float(line[9:19].strip())
+            # Spin-parity
+            JP_p = line[21:39].strip()
+            # Half life with unit
+            T12_p = line[39:49].strip().lower()
+            # Q-value in keV
+            Q_p = float(line[64:74].strip())
+
+            parent = {"E":E_p, "JP":JP_p, "T12":T12_p, "Q":Q_p}
+
         # Look for a level record
         if rec == "L":
-            # Energy in KeV
+            # Energy in keV
             E = float(line[9:19].strip())
             # Spin-parity
             JP = line[21:39].strip()
@@ -71,12 +81,9 @@ def parse_ensdf_levels_gammas(path, nucid):
 
             levels[E]["G"].append({"Eg":Eg, "Ig":Ig, "M":M, "MR":MR})
 
-    # Make a list of all levels in order
-    all_levels = sorted(levels.keys(), reverse=True)
-
     # print(levels)
     # print(all_levels)
-    return levels, all_levels
+    return levels, parent
 
 
 def limit_intensity(levels, I_min=0):
@@ -104,7 +111,7 @@ def limit_intensity(levels, I_min=0):
     return new_levels, new_all_levels
 
 
-def plot_decay_scheme(levels, all_levels, parent="??", daughter="??"):
+def plot_decay_scheme(levels, all_levels, parent, parent_name="??", daughter_name="??", q_text=""):
     plt.close('all')
     inch_to_mm = 25.4
     color = plt.cm.tab10
@@ -129,12 +136,12 @@ def plot_decay_scheme(levels, all_levels, parent="??", daughter="??"):
     x_min = 0.0
     x_max = 1.0
     x_gamma_min = 0.2
-    x_gamma_max = 0.6
+    x_gamma_max = 0.55
     x_step = (x_gamma_max - x_gamma_min) / (number_of_gammas - 1)
     x_pos = x_gamma_min
 
     x_pad_left = 0.3
-    x_pad_right = 0.3
+    x_pad_right = 0.25
     ax.set_xlim([-x_pad_left*x_max, (1+x_pad_right)*x_max])
 
     # for key, value in levels.items():
@@ -163,14 +170,14 @@ def plot_decay_scheme(levels, all_levels, parent="??", daughter="??"):
             ax.annotate(None, xy=(x_pos, E-Eg), xytext=(x_pos, E), 
                         arrowprops=dict(arrowstyle="->", lw=1.0, shrinkA=0, shrinkB=0),
                         zorder=3)
-            ax.text(x_pos, E, f"{Eg:.2f} keV, {Ig}\%",
-                    rotation=70, rotation_mode="anchor", ha="left", va="bottom", fontsize=8,
+            ax.text(x_pos, E+0.005*energy_span, f"{Eg:.2f} keV {Ig}\%",
+                    rotation=60, rotation_mode="anchor", ha="left", va="bottom", fontsize=8,
                     bbox=dict(boxstyle="square,pad=0.01", fc="white", ec="none"),
                     zorder=4)
             
             x_pos += x_step
 
-        x_pos_decay_arrow_left = - x_pad_left * x_max + 0.02
+        x_pos_decay_arrow_left = -x_pad_left * x_max + 0.04
         x_pos_decay_arrow_right = -0.01
         ax.annotate(None, xy=(x_pos_decay_arrow_right, E), xytext=(x_pos_decay_arrow_left, E), 
                     arrowprops=dict(arrowstyle="->", lw=1.0, shrinkA=0, shrinkB=0),
@@ -178,25 +185,44 @@ def plot_decay_scheme(levels, all_levels, parent="??", daughter="??"):
         ax.text(x_pos_decay_arrow_right-0.04, E, f"{Idec}\%", ha="right", va="bottom", fontsize=8, zorder=4)
         ax.text(x_min+0.01, E, f"${JP}$", ha="left", va="bottom", fontsize=8, zorder=4)
     
-    y_pos_arrow = (1 + y_pad_over - 0.1) * energy_span
+    y_pad_over_space = 0.15
+    y_pos_arrow = (1 + y_pad_over - y_pad_over_space) * energy_span
     ax.annotate(None, xy=(x_pos_decay_arrow_left, 0.0), xytext=(x_pos_decay_arrow_left, y_pos_arrow), 
                 arrowprops=dict(arrowstyle="-", lw=1.0, shrinkA=0, shrinkB=0),
                 zorder=4)
-    ax.annotate(None, xy=(x_pos_decay_arrow_right, y_pos_arrow), xytext=(x_pos_decay_arrow_left, y_pos_arrow), 
-                arrowprops=dict(arrowstyle="-", lw=2.0, shrinkA=0, shrinkB=0),
-                zorder=4)
+    ax.hlines(y_pos_arrow, x_pos_decay_arrow_left-0.003, x_pos_decay_arrow_right, color="black", linewidth=2.0, zorder=4)
 
-    ax.text(0.5, 0.0-0.03*energy_span, daughter, ha="center", va="top", fontsize=10, zorder=4)
-    ax.text((x_pos_decay_arrow_left+x_pos_decay_arrow_right)/2, y_pos_arrow-0.03*energy_span, parent, ha="center", va="top", fontsize=10, zorder=4)
+    ax.text(0.5, 0.0-0.03*energy_span, daughter_name, ha="center", va="top", fontsize=10, zorder=4)
+    ax.text((x_pos_decay_arrow_left+x_pos_decay_arrow_right)/2, y_pos_arrow-0.03*energy_span, parent_name, ha="center", va="top", fontsize=10, zorder=4)
+
+    E_p = parent["E"]
+    JP_p = parent["JP"]
+    T12_p = parent["T12"]
+    Q_p = parent["Q"]
+    ax.text(x_pos_decay_arrow_left+0.01, y_pos_arrow, f"{JP_p}", ha="left", va="bottom", fontsize=8, zorder=4)
+    ax.text(x_pos_decay_arrow_right-0.01, y_pos_arrow, f"{E_p}", ha="right", va="bottom", fontsize=8, zorder=4)
+    ax.text((x_pos_decay_arrow_left+x_pos_decay_arrow_right)/2, y_pos_arrow+0.06*energy_span, f"{T12_p}", ha="center", va="bottom", fontsize=8, zorder=4)
+    ax.text(x_pos_decay_arrow_left, y_pos_arrow-(y_pad_over-y_pad_over_space)/2*energy_span, f"//",
+            rotation=90, rotation_mode="anchor", ha="center", va="center", fontsize=6,
+            bbox=dict(boxstyle="square,pad=0.01", fc="white", ec="none"),
+            zorder=4)
+    ax.text(x_pos_decay_arrow_left+0.03, y_pos_arrow-(y_pad_over-y_pad_over_space)/2*energy_span, f"{q_text}={Q_p} keV", ha="left", va="center", fontsize=8, zorder=4)
+    # ax.text((x_pos_decay_arrow_left+x_pos_decay_arrow_right)/2, y_pos_arrow-0.2*energy_span, f"{q_text}{Q_p} keV", ha="center", va="bottom", fontsize=8, zorder=4)
+
+    save_name = "figure"
+    # plt.savefig(f'figures/{save_name}.jpg', dpi=600)
+    plt.savefig(f'figures/{save_name}.pdf')
 
     plt.show()
 
 
-levels, all_levels = parse_ensdf_levels_gammas("ensdf_files/140la.txt", nucid="140CE")
+levels, parent = parse_ensdf_levels_gammas("ensdf_files/140la.txt", nucid="140CE")
 levels, all_levels = limit_intensity(levels, I_min=10)
 
 print(levels)
+print(parent)
 
-plot_decay_scheme(levels, all_levels, 
-                  parent=r"$\mathrm{\prescript{140}{57}{La}}$", 
-                  daughter=r"$\mathrm{\prescript{140}{56}{Ce}}$")
+plot_decay_scheme(levels, all_levels, parent,
+                  parent_name=r"$\mathrm{\prescript{140}{57}{La}}$", 
+                  daughter_name=r"$\mathrm{\prescript{140}{56}{Ce}}$",
+                  q_text=r"$Q_{\beta\text{-}}$")
